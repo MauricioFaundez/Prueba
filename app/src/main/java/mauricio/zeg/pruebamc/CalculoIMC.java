1c078b8ae85825c0032b6cb44758c99e3c4f270c
Mauri <m.faundez11@alumnos.santotomas.cl>
2021-09-04 11:09:37 -0400

    Hola curso

---
+++ app/src/main/java/mauricio/zeg/pruebamc/CalculoIMC.java
@@ -0,0 +1,14 @@
+package mauricio.zeg.pruebamc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculoIMC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo_imc);
+    }
+}
